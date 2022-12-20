11d45145f9967b1a7a62f2dabb8ed113eb4d64c7
wendelmatheus <wendeltel6a@gmail.com>
2022-12-20 12:17:40 -0300

    Atualização Código

--- Lâmpada/assets/script.js
+++ Lâmpada/assets/script.js
@@ -1,21 +1,35 @@
 const imagem = document.querySelector("img#lampada")
 
+/*
+    criação dos eventos do mouse:
+
+    mouseover: quando o mouse entra
+    mouseout: quando o mouse sai
+    click: quando o mouse clica
+
+*/
 imagem.addEventListener('mouseover', ligar)
 imagem.addEventListener('mouseout', desligar)
 imagem.addEventListener('click', quebrar)
-// imagem.addEventListener('click', isLampBroken)
 
+// criação da função para verificar se a lâmpada está quebrada
 function isLampBroken() {
     return imagem.src.indexOf('quebrada') > -1
 }
 
+// função para ligar a lâmpada
+
+/*
+    "!" é negação, ou seja, se aquela condição for falsa
+    OBS: !isLampBroken é a condição se a lâmpada não estiver quebrada... então executa aquele pedaço de código dentro do if
+    */
 function ligar() {
     if(!isLampBroken()) {
         imagem.setAttribute("src", "assets/imagens/ligada.jpg")
     }
 }
     
-
+// função para desligar a lâmpada
 function desligar() {
     if(!isLampBroken()) {
         imagem.setAttribute("src", "assets/imagens/desligada.jpg")
@@ -23,8 +37,8 @@ function desligar() {
     
 }
 
+// função para quebrar a lâmpada
 function quebrar() {
     imagem.setAttribute("src", "assets/imagens/quebrada.jpg")
-
 }
 
